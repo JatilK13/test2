d7001051ef6a0408793d3f3a9b584c556f9a6a83
Jatil Kapadia <jatilkapadia@Jatils-MacBook-Air.local>
2024-04-09 23:54:13 -0400

    new edit

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Elixir? Smalltalk? HaskellRust!?");
+        System.out.println("Aomine Daiki is a fraud");
+    }
+}
